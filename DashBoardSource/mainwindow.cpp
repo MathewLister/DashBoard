04651c31fe8f0cb67558a23b3a08bcfe8307d548
AngryPorkchop11 <listermathew0@gmail.com>
2020-02-18 14:30:51 -0800

    Added clock and greeting messages

--- DashBoardSource/mainwindow.cpp
+++ DashBoardSource/mainwindow.cpp
@@ -4,12 +4,53 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , timer(new QTimer)
 {
     ui->setupUi(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(setCurrentTime()));
+    setCurrentTime();
+    timer->start(1000);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setCurrentTime()
+{
+    QTime time = QTime::currentTime();
+    QString hour = time.toString("hh");
+    QString min = time.toString("mm");
+
+    //Good evening!
+    if(hour.toInt() == 17 || hour.toInt() <= 24) {
+        ui->Greeting->setText("Good evening!");
+    }
+
+    //Good afternoon!
+    if(hour.toInt() == 12 || hour.toInt() <= 16) {
+        ui->Greeting->setText("Good afternoon!");
+    }
+
+    //Good morning!
+    if(hour.toInt() == 7 || hour.toInt() <= 11) {
+        ui->Greeting->setText("Good morning!");
+    }
+
+    //Go to bed!
+    if(hour.toInt() == 0 || hour.toInt() <= 6) {
+        ui->Greeting->setText("Go to bed!");
+    }
+
+    //Make the clock 12 hrs instead of 24
+    int twelveHour = hour.toInt();
+    if (twelveHour > 12) {
+        twelveHour = twelveHour - 12;
+        hour = QString::number(twelveHour);
+        ui->ampm->setText("pm");
+    }
+    ui->Hours->display(hour);
+    ui->Mins->display(min);
 }
 
---
+++ build-DashBoardSource-Desktop_Qt_5_12_6_MSVC2017_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,95 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLCDNumber>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QLCDNumber *Hours;
+    QLCDNumber *Mins;
+    QLabel *ampm;
+    QLabel *Greeting;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(1290, 1026);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        Hours = new QLCDNumber(centralwidget);
+        Hours->setObjectName(QString::fromUtf8("Hours"));
+        Hours->setGeometry(QRect(40, 100, 131, 101));
+        QFont font;
+        font.setFamily(QString::fromUtf8("MS Serif"));
+        Hours->setFont(font);
+        Hours->setDigitCount(2);
+        Hours->setProperty("value", QVariant(22.000000000000000));
+        Mins = new QLCDNumber(centralwidget);
+        Mins->setObjectName(QString::fromUtf8("Mins"));
+        Mins->setGeometry(QRect(180, 100, 131, 101));
+        Mins->setDigitCount(2);
+        Mins->setProperty("value", QVariant(22.000000000000000));
+        ampm = new QLabel(centralwidget);
+        ampm->setObjectName(QString::fromUtf8("ampm"));
+        ampm->setGeometry(QRect(320, 180, 69, 20));
+        QFont font1;
+        font1.setPointSize(10);
+        ampm->setFont(font1);
+        Greeting = new QLabel(centralwidget);
+        Greeting->setObjectName(QString::fromUtf8("Greeting"));
+        Greeting->setGeometry(QRect(50, 40, 251, 41));
+        QFont font2;
+        font2.setPointSize(12);
+        Greeting->setFont(font2);
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 1290, 29));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        ampm->setText(QApplication::translate("MainWindow", "am", nullptr));
+        Greeting->setText(QApplication::translate("MainWindow", "Hi!", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
